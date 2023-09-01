d42235d186932eab3f4efa5644ddd56d285f1ab8
rohitGupta2022 <pta.rohit28@gmail.com>
2023-09-01 15:27:34 +0530

    Add: folder structure

---
+++ app/(auth)/(routes)/login/page.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+export interface pageProps { }
+
+const page: FC<pageProps> = ({ }) => {
+  return (
+    <div>
+      page
+    </div>
+  );
+};
+export default page 
---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div>{children}</div>
+  )
+}
