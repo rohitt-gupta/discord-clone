cc38a7b6bc0bdd48cfaf724287ec06ae4cee6d87
rohitGupta2022 <pta.rohit28@gmail.com>
2023-12-31 13:00:31 +0530

    Add: Left Navigation Bar

---
+++ components/navigation/NavigationSidebar.tsx
@@ -0,0 +1,66 @@
+import { currentProfile } from "@/lib/current-profile";
+import db from "@/lib/db";
+import { redirect } from "next/navigation";
+import { FC } from "react";
+import { NavigationAction } from "./NavigationAction";
+import { Separator } from "../ui/separator";
+import { ScrollArea } from "../ui/scroll-area";
+import { NavigationItem } from "./NavigationItem";
+import { ModeToggle } from "../ModeToggle";
+import { dark } from "@clerk/themes";
+import { UserButton } from "@clerk/nextjs";
+
+export interface NavigationSidebarProps { }
+
+const NavigationSidebar: FC<NavigationSidebarProps> = async ({ }) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirect('/')
+  }
+
+  const servers = await db.server.findMany({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id
+        }
+      }
+    }
+  })
+
+
+
+  return (
+    <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1e1f22] py-3">
+      <NavigationAction />
+      <Separator
+        className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto"
+      />
+      <ScrollArea className="flex-1 w-full" >
+        {servers.map((server) => (
+          <div key={server.id} className="mb-4">
+            <NavigationItem
+              id={server.id}
+              name={server.name}
+              imageUrl={server.imageUrl}
+            />
+          </div>
+        ))}
+      </ScrollArea>
+      <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
+        <ModeToggle />
+        <UserButton
+          afterSignOutUrl="/"
+          appearance={{
+            baseTheme: dark,
+            elements: {
+              avatarBox: "h-[48px] w-[48px]"
+            }
+          }}
+        />
+      </div>
+    </div >
+  );
+};
+export default NavigationSidebar 
