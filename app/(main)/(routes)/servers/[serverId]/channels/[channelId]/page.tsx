90910f2e8ffe3ba50ee0d05aaa9157075da69b33
rohitGupta2022 <pta.rohit28@gmail.com>
2024-01-04 22:20:24 +0530

    Add: Mobile toggle in chat header

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import ChatHeader from "@/components/chat/ChatHeader";
+import { currentProfile } from "@/lib/current-profile";
+import db from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { FC } from "react";
 
-const ChannelIdPage = () => {
-  return (
-    <div>Channel ID Page</div>
-  )
+export interface ChannelIdPageProps {
+  params: {
+    serverId: string;
+    channelId: string;
+  }
 }
 
-export default ChannelIdPage
+const ChannelIdPage: FC<ChannelIdPageProps> = async ({ params: { channelId, serverId } }) => {
+
+  const profile = await currentProfile()
+
+  if (!profile) return redirectToSignIn()
+
+  const channel = await db.channel.findUnique({
+    where: {
+      id: channelId,
+    }
+  })
+  const member = await db.member.findFirst({
+    where: {
+      serverId: serverId,
+      profileId: profile.id
+    }
+  })
+
+
+  if (!channel || !member) return redirect('/')
+  return (
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader
+        name={channel.name}
+        serverId={channel.serverId}
+        type="channel"
+      />
+    </div>
+  );
+};
+export default ChannelIdPage 
---
+++ components/chat/ChatHeader.tsx
@@ -0,0 +1,27 @@
+import { Hash, Menu } from "lucide-react";
+import { FC } from "react";
+import { MobileToggle } from "../MobileToggle";
+
+export interface ChatHeaderProps {
+  serverId: string;
+  name: string;
+  type: "channel" | "conversation";
+  imageUrl?: string;
+}
+
+const ChatHeader: FC<ChatHeaderProps> = ({
+  serverId, name, type, imageUrl
+}) => {
+  return (
+    <div className="text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
+      <MobileToggle serverId={serverId} />
+      {type === "channel" && (
+        <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
+      )}
+      <p className="font-semibold text-md text-black dark:text-white">
+        {name}
+      </p>
+    </div>
+  );
+};
+export default ChatHeader 
